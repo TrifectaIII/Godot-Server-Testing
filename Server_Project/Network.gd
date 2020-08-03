89cec18d65a557e008118d1e1550edb2fac4089e
Dakota Madden-Fong <maddenfong@gmail.com>
2020-08-02 19:18:34 -0700

    work on tutorial project

--- Server_Project/Network.gd
+++ Server_Project/Network.gd
@@ -1,4 +1,13 @@
 extends Node
 
 
-const DEFAULT
+const DEFAULT_IP = '127.0.0.1'
+const DEFAULT_PORT = 8000
+const MAX_PLAYERS = 5
+
+var players = {}
+
+var self_data = {name = "", position = Vector2(360,180)}
+
+func _ready():
+    pass
